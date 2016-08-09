732bd9fb043ac21098c608b76f39f8ac27529506
Marijn Pool <marijn@marijnpool.com>
2016-08-09 20:49:28 +0200

    About app added

---
+++ apps/about/resources/js/app-settings.js
@@ -0,0 +1,12 @@
+$('#settings').onstart(function() {
+  // Define stuff on start
+});
+$('#settings').ondestroy(function() {
+  // Define stuff on destroy
+});
+settings.actionOne = function() {
+  Droid.toast('kill');
+}
+settings.actionTwo = function() {
+  Droid.toast('kees');
+}
---
+++ apps/about/resources/views/default.php
@@ -0,0 +1,11 @@
+<!-- Default view -->
+<section id="default-view">
+  <?php
+    Droid::renderAppBar($app->name, $app->colors->colorPrimary, $app->colors->colorText, $app->colors->colorIcon, null);
+  ?>
+  <h3>Online Android Simulator is developed by:</h3><br />
+  <p>Wouter Molhoek</p>
+  <p>Peter Christiaanse</p>
+  <p><a href="http://rafaelklaessen.nl" target="_blank">Rafael Klaessen</a></p>
+  <p><a href="http://marijnpool.com" target="_blank">Marijn Pool</a></p>
+</section>
