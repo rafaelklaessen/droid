2f8f76d50a1c3df34fdb66160d4d8685dc79b031
rafaelklaessen <rafaelklaessen.nl@gmail.com>
2016-08-09 16:48:27 +0200

    Chrome search bar animation

--- apps/chrome/resources/js/app-chrome.js
+++ apps/chrome/resources/js/app-chrome.js
@@ -1,4 +1,10 @@
 $('#chrome').onstart(function() {
+  $('#chrome .chromesearchbar').focus(function() {
+    $('#chrome .app-name').addClass('fullwidth');
+  });
+  $('#chrome .chromesearchbar').blur(function() {
+    $('#chrome .app-name').removeClass('fullwidth');
+  });
   $(this).keypress(function(e) {
     // Enter button is pressed, set iframe url
     if (e.which == 13) {
