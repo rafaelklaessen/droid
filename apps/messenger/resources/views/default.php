36b32bd3f8066ab08b3dc6dfa53e02d21116de47
Marijn Pool <marijn@marijnpool.com>
2016-08-09 21:05:23 +0200

    Added phone and messenger

---
+++ apps/messenger/resources/views/default.php
@@ -0,0 +1,6 @@
+<!-- Default view -->
+<section id="default-view">
+  <?php
+    Droid::renderAppBar($app->name, $app->colors->colorPrimary, $app->colors->colorText, $app->colors->colorIcon, null);
+  ?>
+</section>
