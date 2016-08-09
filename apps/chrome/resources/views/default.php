b2bd40ba3442e509ec18a04db694cd77ce812be9
rafaelklaessen <rafaelklaessen.nl@gmail.com>
2016-08-09 15:32:21 +0200

    Update da view
    Update da view

--- apps/chrome/resources/views/default.php
+++ apps/chrome/resources/views/default.php
@@ -1,10 +1,7 @@
 <!-- Default view -->
 <section id="default-view">
   <?php
-<<<<<<< HEAD
     $list = '';
-=======
->>>>>>> origin/master
     Droid::renderAppBar($app->name, $app->colors->colorPrimary, $app->colors->colorText, $list);
   ?>
   doei
