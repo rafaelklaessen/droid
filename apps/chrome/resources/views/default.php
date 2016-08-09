8b731d668f0105f65a343ee405c59d3b3c2413e0
Marijn Pool <marijn@marijnpool.com>
2016-08-09 15:26:04 +0200

    menu eruit gehaald

--- apps/chrome/resources/views/default.php
+++ apps/chrome/resources/views/default.php
@@ -1,14 +1,6 @@
 <!-- Default view -->
 <section id="default-view">
   <?php
-    $list = Droid::makeList(array(
-      'Item' => array(
-        'onclick' => 'chrome.actionOne()'
-      ),
-      'Item2' => array(
-        'onclick' => 'chrome.actionTwo()'
-      )
-    ));
     Droid::renderAppBar($app->name, $app->colors->colorPrimary, $app->colors->colorText, $list);
   ?>
   doei
