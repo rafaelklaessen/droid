23b5fab7320714ee5e90179021282e6246ecea40
rafaelklaessen <rafaelklaessen.nl@gmail.com>
2016-08-09 16:35:02 +0200

    Updated search bar
    Updated search bar

--- apps/chrome/resources/js/app-chrome.js
+++ apps/chrome/resources/js/app-chrome.js
@@ -1,6 +1,14 @@
 $('#chrome').onstart(function() {
-  // Define stuff on start
+  $(this).keypress(function(e) {
+    // Enter button is pressed, set iframe url
+    if (e.which == 13) {
+      var url = $('#chrome .chromesearchbar').val();
+      $('#chrome .chromeframe').attr('src', url);
+    }
+  });
 });
 $('#chrome').ondestroy(function() {
   // Define stuff on destroy
 });
+
+setInterval(function() {console.log($(window).scrollTop());}, 1);
--- apps/chrome/resources/views/default.php
+++ apps/chrome/resources/views/default.php
@@ -1,7 +1,7 @@
 <!-- Default view -->
 <section id="default-view">
   <?php
-    Droid::renderAppBar('<input type="url" placeholder="Search or type URL">', $app->colors->colorPrimary, $app->colors->colorText, null);
+    Droid::renderAppBar('<input tabindex="-1" class="chromesearchbar" type="url" placeholder="Search or type URL">', $app->colors->colorPrimary, $app->colors->colorText, null);
   ?>
   <iframe width="100%" height="100%" class="chromeframe" src="http://marijnpool.com"></iframe>
 </section>
