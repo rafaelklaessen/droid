9fcf27ed8011d39d84fc88cf19f7942f5d500bc5
Marijn Pool <marijn@marijnpool.com>
2016-08-11 00:30:55 +0200

    Added WhatsApp
    It’s a shame they blocked embedding otherwise we could have a real
    WhatsApp build in. Maybe we do something with a bot later.

---
+++ apps/whatsapp/app.php
@@ -0,0 +1,10 @@
+<!-- App CSS -->
+<link rel="stylesheet" type="text/css" href="<?php echo $appLocation; ?>/resources/css/app-whatsapp.css">
+<!-- App JavaScript -->
+<script type="text/javascript" src="<?php echo $appLocation; ?>/resources/js/app-whatsapp.js"></script>
+<?php
+  /**
+    * App file. Include all views
+    */
+  include $appLocation . '/resources/views/default.php';
+?>
---
+++ apps/whatsapp/resources/views/default.php
@@ -0,0 +1,7 @@
+<!-- Default view -->
+<section id="default-view">
+  <?php
+    Droid::renderAppBar($app->name, $app->colors->colorPrimary, $app->colors->colorText, $app->colors->colorIcon, null);
+  ?>
+  WhatsApp accepteerd geen embed dus werkt niet beetje jammer dit.
+</section>
